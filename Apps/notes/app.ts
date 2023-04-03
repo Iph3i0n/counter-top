ba2ba3b7e959e29c8cb3dc2e2c6c5339cb89f700
Paul Popat <paul@popatux.co.uk>
2023-04-03 13:05:35 +0000

    Improved the notes app

--- Apps/notes/app.ts
+++ Apps/notes/app.ts
@@ -1,12 +1,5 @@
 import CreateAppServer from "../../Interfacing/AppServer.ts";
-import {
-  Struct,
-  ASCII,
-  UTF8,
-  DateTime,
-  Array,
-  Optional,
-} from "../../deps/moulding_tin.ts";
+import { Struct, UTF8, DateTime } from "../../deps/moulding_tin.ts";
 
 const Server = CreateAppServer(
   {
@@ -19,7 +12,64 @@ const Server = CreateAppServer(
   },
   {},
   (c) => {
-    c.OpenWindow("index.html", "File Explorer").then(() => c.EndApp());
+    c.OpenWindow("index.html", "Notes").then(() => c.EndApp());
     return c;
   }
 );
+
+Server.CreateHandler("create_note", ({ UserState }, _, name: string) => {
+  const id = crypto.randomUUID();
+  UserState.Write({
+    notes: {
+      [id]: {
+        name,
+        created: new Date(),
+        modified: new Date(),
+        text: "",
+      },
+    },
+  });
+
+  return id;
+});
+
+Server.CreateHandler(
+  "update_note",
+  ({ UserState }, _, id: string, name: string, text: string) => {
+    const existing = UserState.Model.notes[id];
+    if (!existing) return "not found";
+
+    UserState.Write({
+      notes: {
+        [id]: {
+          name,
+          created: existing.modified,
+          modified: new Date(),
+          text,
+        },
+      },
+    });
+  }
+);
+
+Server.CreateHandler("delete_note", ({ UserState }, _, id: string) => {
+  const existing = UserState.Model.notes[id];
+  if (!existing) return "not found";
+
+  UserState.Write({ notes: { [id]: undefined } });
+});
+
+Server.CreateHandler("notes_list", ({ UserState }, _) => {
+  const result = [];
+  for (const [id, value] of UserState.Model.notes)
+    result.push({ id, name: value.name, modified: value.modified });
+
+  return result;
+});
+
+Server.CreateHandler("note_text", ({ UserState }, _, id: string) => {
+  const existing = UserState.Model.notes[id];
+  if (!existing) return "not found";
+
+  return existing.text;
+});
